b945c598374d388fb834bf714a6663ff36cf6a17
aditya-eko <ekoagusaditya@gmail.com>
2019-09-15 16:43:48 +0700

    menambahkan file crud.py

--- crud.py
+++ crud.py
@@ -1,10 +1,41 @@
-mahasiswa=["eko","agus"]
-
-masukan=str(input("masukan nama"))
+mahasiswa=[]
 
 def tambah():
-    return mahasiswa.append(masukan)
+    masukan=str(input("masukan nama mahasiswa: "))
+    global mahasiswa
+    # mahasiswa=[]
+    mahasiswa+=[masukan]
+    return masukan
 
 
+def kurang():
+    masukan=str(input("masukan nama yang akan di hapus: "))
+    global mahasiswa
+    mahasiswa.remove(masukan)
+    return mahasiswa
+
+
+def edit():
+    masukan=str(input("nama yang akan di edit: "))
+    masukan2=str(input("masukan nama baru: "))
+    global mahasiswa
+    masukan2=mahasiswa.append(masukan2)
+    return mahasiswa
+
+
+# menambah dengan fungsi
+print(mahasiswa)
 tambah()
+print(mahasiswa)
+tambah()
+print(mahasiswa)
+
+
+
+# mengurangi mahasiswa dengan fungsi
+
+kurang()
+print(mahasiswa)
+
+edit()
 print(mahasiswa)
